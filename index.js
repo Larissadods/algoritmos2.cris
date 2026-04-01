9d93356484f710b3aa84e0150b674294b83ad982
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2026-04-01 09:53:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const { edGalho, edFolha } = require('./arrays');
+const listaLivros = require('./array');
 
-function juntaListas(lista1, lista2) {
-  let listaFinal = [];
-  let posicaoAtualLista1 = 0;
-  let posicaoAtualLista2 = 0;
-  let atual = 0;
+function mergeSort(array, nivelAninhamento = 0) {
 
-  while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
-    let produtoAtualLista1 = lista1[posicaoAtualLista1];
-    let produtoAtualLista2 = lista2[posicaoAtualLista2];
+  console.log(`nível de aninhamento: ${nivelAninhamento}`)
+  console.log(array)
 
-    if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
-      listaFinal[atual] = produtoAtualLista1;
-      posicaoAtualLista1++;
-    } else {
-      listaFinal[atual] = produtoAtualLista2;
-      posicaoAtualLista2++;
-    }
-
-    atual++;
+  if(array.length > 1) {
+    const meio = Math.floor(array.length / 2);
+    const parte1 = mergeSort(array.slice(0, meio), nivelAninhamento + 1);
+    const parte2 = mergeSort(array.slice(meio, array.length), nivelAninhamento + 1);
+    array = ordena(parte1, parte2);
   }
 
-  while (posicaoAtualLista1 < lista1.length) {
-    listaFinal[atual] = lista1[posicaoAtualLista1];
-    posicaoAtualLista1++;
-    atual++;
-  }
-
-  while (posicaoAtualLista2 < lista2.length) {
-    listaFinal[atual] = lista2[posicaoAtualLista2];
-    posicaoAtualLista2++;
-    atual++;
-  }
-
-  return listaFinal;
+  return array;
 }
 
-console.log(juntaListas(edGalho, edFolha))
+function ordena(parte1, parte2) {
+  let posicaoAtualParte1 = 0 
+  let posicaoAtualParte2 = 0
+  const resultado = []
+
+  while (posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length) {
+    let produtoAtualParte1 = parte1[posicaoAtualParte1]
+    let produtoAtualParte2 = parte2[posicaoAtualParte2]
+
+    if (produtoAtualParte1.preco < produtoAtualParte2.preco) {
+      resultado.push(produtoAtualParte1)
+      posicaoAtualParte1++
+    } else {
+      resultado.push(produtoAtualParte2)
+      posicaoAtualParte2++
+    }
+  }
+
+  return resultado.concat(posicaoAtualParte1 < parte1.length
+    ? parte1.slice(posicaoAtualParte1)
+    : parte2.slice(posicaoAtualParte2))
+}
+
+console.log(mergeSort(listaLivros));
